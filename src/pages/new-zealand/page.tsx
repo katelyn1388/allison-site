4af89e464afa7573774cb3e29db7ee0468504b44
bowerska <katelyn.bowers@lcms.org>
2024-10-21 17:29:14 -0500

    Added haircutting services to services page

--- src/pages/new-zealand/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AppLayout } from '../layout';
-
-export default function Page() {
-  return (
-    <AppLayout title="New Zealand">
-      <div className="d-print-none"></div>
-    </AppLayout>
-  );
-}
--- src/pages/services/haircutting.tsx
+++ src/pages/services/haircutting.tsx
@@ -2,10 +2,71 @@ import { DivImage } from '../../components/div-image';
 
 export function Haircutting() {
     return (
-        <div className='haircutting'>
+        <div className='haircutting-section w-100 mx-5 w-100 mt-5'>
             <h2>Haircutting</h2>
-            <div>
+            <div className='d-flex justify-content-around w-100'>
                 <DivImage height='30rem' width='30rem' img='/images/frank.png' smallClass='haircutting' />
+                <div className='d-flex flex-column w-75 ms-2'>
+                    <div className='d-flex flex-column justify-content-center text-center mt-3'>
+                        <div className='d-flex flex-column justify-content-center'>
+                            <span>
+                                <b>New Guest Curly Cut: $175</b>
+                            </span>
+                            <small>
+                                This is an in-depth service where we will educate you on all things curls! This service includes a
+                                consultation about your hair goals, a curly cut, hair detox, personalized treatment, in-depth styling
+                                education, and a styling routine sheet to take home
+                            </small>
+                        </div>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center text-center mt-3'>
+                        <div className='d-flex flex-column justify-content-center'>
+                            <span>
+                                <b>Repeat Guest Curly Cut: $125</b>
+                            </span>
+                            <small>
+                                This service is for guests who have had an appointment in the last six months. This service includes a
+                                consultation, a curly cut, wash and style, and styling tips
+                            </small>
+                        </div>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center text-center mt-3'>
+                        <div className='d-flex flex-column justify-content-center'>
+                            <span>
+                                <b>Express Curly Cut: $90</b>
+                            </span>
+                            <small>
+                                This service is for returning guests who only want a curly cut and do not need a wash and style.{' '}
+                                <b>When you arrive to your appointment, your hair must be down and freshly styled. </b>
+                                This service includes a consultation and curly cut. This service <b>does not</b> include a wash and style
+                            </small>
+                        </div>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center text-center mt-3'>
+                        <div className='d-flex flex-column justify-content-center'>
+                            <span>
+                                <b>New Guest Curly Lesson: $75</b>
+                            </span>
+                            <small>
+                                This service is for guests who want to learn how to love and take care of their curls.{' '}
+                                <b>This service does not include a curly cut.</b> This service includes an in-depth consultation on your
+                                hair goals, ahair detox treatment, a treatment personalized for your hair, product recommendations, curly
+                                education, and a care sheet to take home
+                            </small>
+                        </div>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center text-center mt-3'>
+                        <div className='d-flex flex-column justify-content-center'>
+                            <span>
+                                <b>returning Guest Curly Set: $55</b>
+                            </span>
+                            <small>
+                                This service is for <b>returning guests only</b> who need their hair styled for an event or simply do not
+                                want to wash their hair at home. This service includes a consultation and a curly style
+                            </small>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
