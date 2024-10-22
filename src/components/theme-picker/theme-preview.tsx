61cf8893d64c90082ca4e68f9fee65caf77bbc0d
bowerska <katelyn.bowers@lcms.org>
2024-10-22 16:19:57 -0500

    Changed name

--- src/components/theme-picker/theme-preview.tsx
+++ src/components/theme-picker/theme-preview.tsx
@@ -2,64 +2,63 @@ import { CustomTheme } from '../../types/custom-theme';
 import ImageUnloaded from '../loaders/image-unloaded';
 
 export function ThemePreview({ primary, background, text }: CustomTheme) {
-  return (
-    <div className="border border-dark rounded w-75" style={{ backgroundColor: background, overflow: 'hidden' }}>
-      <div className="row w-100 ps-4" style={{ backgroundColor: primary, top: '0', marginLeft: '.01rem', color: text }}>
-        <h5 className="text-center">Katelyn Bowers the Cool Kid</h5>
-        <div className="d-flex justify-content-around">
-          <h6 style={{ opacity: '70%' }}>Home</h6>
-          <h6>Book Appointment</h6>
-          <h6>Recommendations</h6>
-          <h6>New Zealand</h6>
-          <h6>Services</h6>
-          <h6>Cool Stuff</h6>
+    return (
+        <div className='border border-dark rounded w-75' style={{ backgroundColor: background, overflow: 'hidden' }}>
+            <div className='row w-100 ps-4' style={{ backgroundColor: primary, top: '0', marginLeft: '.01rem', color: text }}>
+                <h5 className='text-center'>Poopy Butt Salon</h5>
+                <div className='d-flex justify-content-around'>
+                    <h6 style={{ opacity: '70%' }}>Home</h6>
+                    <h6>Home</h6>
+                    <h6>Services</h6>
+                    <h6>Book Appointment</h6>
+                    <h6>Recommendations</h6>
+                </div>
+            </div>
+            <div className='ms-4' style={{ backgroundColor: background, height: '20rem' }}>
+                <h4>Kbow Travels</h4>
+                <img src='https://lh3.googleusercontent.com/d/1S1ClaQgrYTBS7gbR8hRjJTCbW4Ov9ffr=s400' alt='Sick Pic' width={'80rem'} />
+                <h5 className='mt-3'>Pictures</h5>
+                <div className='d-flex justify-content-around'>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                </div>
+                <div className='d-flex justify-content-around mt-2'>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                    <div style={{ width: '5rem' }}>
+                        <ImageUnloaded />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="ms-4" style={{ backgroundColor: background, height: '20rem' }}>
-        <h4>Kbow Travels</h4>
-        <img src="https://lh3.googleusercontent.com/d/1S1ClaQgrYTBS7gbR8hRjJTCbW4Ov9ffr=s400" alt="Sick Pic" width={'80rem'} />
-        <h5 className="mt-3">Pictures</h5>
-        <div className="d-flex justify-content-around">
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-        </div>
-        <div className="d-flex justify-content-around mt-2">
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-          <div style={{ width: '5rem' }}>
-            <ImageUnloaded />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
