3ff3998f8d54beb6925b3cff27285dd4de2f706a
bowerska <katelyn.bowers@lcms.org>
2024-10-24 15:23:41 -0500

    Removed unused scripts

--- src/routing/routing.tsx
+++ src/routing/routing.tsx
@@ -3,19 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import IndexPage from '../pages/home';
 import BookingPage from '../pages/book';
 import RecommendationsPage from '../pages/recommendations';
-// import NewZealandPage from '../pages/new-zealand';
 import ServicesPage from '../pages/services';
 
 export const Routing = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<IndexPage />} path="/"></Route>
-        <Route element={<ServicesPage />} path="/services"></Route>
-        <Route element={<BookingPage />} path="/bookAppointment"></Route>
-        <Route element={<RecommendationsPage />} path="/recommendations"></Route>
-        {/* <Route element={<NewZealandPage />} path="/newzealand"></Route> */}
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<IndexPage />} path='/'></Route>
+                <Route element={<ServicesPage />} path='/services'></Route>
+                <Route element={<BookingPage />} path='/bookAppointment'></Route>
+                <Route element={<RecommendationsPage />} path='/recommendations'></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
