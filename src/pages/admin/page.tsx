34e9fec99f69629d054b8dda304f234b98caedf7
bowerska <katelyn.bowers@lcms.org>
2024-10-29 14:17:14 -0500

    Added non-working code for server and api

--- src/pages/admin/page.tsx
+++ src/pages/admin/page.tsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
 import { AppLayout } from '../layout';
 import { AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
 
 export default function Page() {
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+        (async function () {
+            const { text } = await (await fetch('/api/message')).json();
+            setData(text);
+        })();
+    }, []);
+
     return (
         <AppLayout title='Admin'>
             <AuthenticatedTemplate>
                 <h1 className='text-center mt-4'>Hey, admin</h1>
+                {data}
             </AuthenticatedTemplate>
             <UnauthenticatedTemplate>
                 <h1 className='text-center mt-4'>Who let you in here?</h1>
